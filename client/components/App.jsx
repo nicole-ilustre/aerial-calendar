4ce6df6d7e8eea98e42c391e7fc03b28ee7885f9
nicole-ilustre <nicoleilustre2292@gmail.com>
2021-04-11 21:10:13 +1200

    schedule component 80% done

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,12 +3,14 @@ import { HashRouter as Router, Route } from 'react-router-dom'
 
 import LogIn from './LogIn'
 import ClassSchedule from './ClassSchedule'
+import Calendar from './Calendar'
 
 const App = () => {
   return (
     <Router>
       <Route exact path='/' component={LogIn}></Route>
-      <Route exact path='/schedule' component={ClassSchedule}></Route>
+      <Route path='/schedule' component={ClassSchedule}></Route>
+      <Route path='/schedule/calendar' component={Calendar}></Route>
     </Router>
   )
 }
--- data/classes.js
+++ data/classes.js
@@ -1,109 +1,122 @@
 export default
-{ Schedule: {
-  Monday: [{
-    title: 'Level 1 Silks',
-    teacher: 'Stacey',
-    description: 'Basic silk techniques for beginners'
+{
+  Schedule: [{
+    Monday: [{
+      title: 'Level 1 Silks',
+      teacher: 'Stacey',
+      description: 'Basic silk techniques for beginners'
+    },
+    {
+      title: 'Level 2 Pole',
+      teacher: 'Rosie',
+      description: 'This class will take you higher up the pole and will go through the basic concept of inverting'
+    },
+    {
+      title: 'Level 1 Hoop',
+      teacher: 'Tash',
+      description: 'Basic poses and transitions on the hoop'
+    }]
   },
   {
-    title: 'Level 2 Pole',
-    teacher: 'Rosie',
-    description: 'This class will take you higher up the pole and will go through the basic concept of inverting'
+    Tuesday: [{
+      title: 'Silks Combos',
+      teacher: 'Ruby',
+      description: 'Stitching moves and tricks together to form combos'
+    },
+    {
+      title: 'Level 3 Pole',
+      teacher: 'Brittney',
+      description: 'Fundamental aerial moves and intermediate tricks'
+    },
+    {
+      title: 'Level 5 Pole',
+      teacher: 'Karry',
+      description: 'The most advanced pole class. Advance tricks and transitions'
+    }]
   },
   {
-    title: 'Level 1 Hoop',
-    teacher: 'Tash',
-    description: 'Basic poses and transitions on the hoop'
-  }],
-  Tuesday: [{
-    title: 'Silks Combos',
-    teacher: 'Ruby',
-    description: 'Stitching moves and tricks together to form combos'
+    Wednesday: [{
+      title: 'Stretch and Flex',
+      teacher: 'Rosie',
+      description: 'Focuses on activation and engagement of muscles to help with flexibility'
+    },
+    {
+      title: 'Spin Pole',
+      teacher: 'Jess',
+      description: 'Tricks, transitions and combos on spin pole'
+    },
+    {
+      title: 'Handstands',
+      teacher: 'Violet',
+      description: 'Work on your handstands skills and conditioning'
+    }]
   },
   {
-    title: 'Level 3 Pole',
-    teacher: 'Brittney',
-    description: 'Fundamental aerial moves and intermediate tricks'
+    Thursday: [{
+      title: 'Heels Flow',
+      teacher: 'Anna',
+      description: 'Build the the art of sensual movement and dance in heels'
+    },
+    {
+      title: 'Conditioning',
+      teacher: 'Kristyna',
+      description: 'All about conditiong to build stronger muscles'
+    },
+    {
+      title: 'Hammock Zen',
+      teacher: 'Danielle',
+      description: 'Time to zen out with a guided meditation on a hhammock'
+    }]
   },
   {
-    title: 'Level 5 Pole',
-    teacher: 'Karry',
-    description: 'The most advanced pole class. Advance tricks and transitions'
-  }],
-  Wednesday: [{
-    title: 'Stretch and Flex',
-    teacher: 'Rosie',
-    description: 'Focuses on activation and engagement of muscles to help with flexibility'
+    Friday: [{
+      title: 'Twerk',
+      teacher: 'Kristyna',
+      description: 'Time to shake and work out your booty with twerk classes'
+    },
+    {
+      title: 'Training time',
+      teacher: 'No instructor',
+      description: 'Free time for students to practice on their own'
+    },
+    {
+      title: 'Floorplay',
+      teacher: 'Nicole',
+      description: 'Learn floorbased tricks and skills and sequences'
+    }]
   },
   {
-    title: 'Spin Pole',
-    teacher: 'Jess',
-    description: 'Tricks, transitions and combos on spin pole'
+    Saturday: [{
+      title: 'Hula Hoop',
+      teacher: 'Olivia',
+      description: 'Learn basic and fun tricks you can do with a Hula Hoop'
+    },
+    {
+      title: 'Training time',
+      teacher: 'No instructor',
+      description: 'Free time for students to practice on their own'
+    },
+    {
+      title: 'Hoop Combos',
+      teacher: 'Ines',
+      description: 'Stitch together tricks and moves on the hoop to beautiful combos'
+    }]
   },
   {
-    title: 'Handstands',
-    teacher: 'Violet',
-    description: 'Work on your handstands skills and conditioning'
-  }],
-  Thursday: [{
-    title: 'Heels Flow',
-    teacher: 'Anna',
-    description: 'Build the the art of sensual movement and dance in heels'
-  },
-  {
-    title: 'Conditioning',
-    teacher: 'Kristyna',
-    description: 'All about conditiong to build stronger muscles'
-  },
-  {
-    title: 'Hammock Zen',
-    teacher: 'Danielle',
-    description: 'Time to zen out with a guided meditation on a hhammock'
-  }],
-  Friday: [{
-    title: 'Twerk',
-    teacher: 'Kristyna',
-    description: 'Time to shake and work out your booty with twerk classes'
-  },
-  {
-    title: 'Training time',
-    teacher: 'No instructor',
-    description: 'Free time for students to practice on their own'
-  },
-  {
-    title: 'Floorplay',
-    teacher: 'Nicole',
-    description: 'Learn floorbased tricks and skills and sequences'
-  }],
-  Saturday: [{
-    title: 'Hula Hoop',
-    teacher: 'Olivia',
-    description: 'Learn basic and fun tricks you can do with a Hula Hoop'
-  },
-  {
-    title: 'Training time',
-    teacher: 'No instructor',
-    description: 'Free time for students to practice on their own'
-  },
-  {
-    title: 'Hoop Combos',
-    teacher: 'Ines',
-    description: 'Stitch together tricks and moves on the hoop to beautiful combos'
-  }],
-  Sunday: [{
-    title: 'Level 1 Pole',
-    teacher: 'Noelle',
-    description: 'Learn the very basic fundamentals of pole'
-  },
-  {
-    title: 'Training time',
-    teacher: 'No instructor',
-    description: 'Free time for students to practice on their own'
-  },
-  {
-    title: 'Hoop Combos',
-    teacher: 'Ines',
-    description: 'Stitch together tricks and moves on the hoop to beautiful combos'
+    Sunday: [{
+      title: 'Level 1 Pole',
+      teacher: 'Noelle',
+      description: 'Learn the very basic fundamentals of pole'
+    },
+    {
+      title: 'Training time',
+      teacher: 'No instructor',
+      description: 'Free time for students to practice on their own'
+    },
+    {
+      title: 'Hoop Combos',
+      teacher: 'Ines',
+      description: 'Stitch together tricks and moves on the hoop to beautiful combos'
+    }]
   }]
-}
 }
