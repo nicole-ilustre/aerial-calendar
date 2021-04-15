2748740a9bf2203cb0360e00190692311b4d7c27
nicole-ilustre <nicoleilustre2292@gmail.com>
2021-04-15 21:51:18 +1200

    made classes show up as a promise in calendar.jsx

--- client/components/Calendar.jsx
+++ client/components/Calendar.jsx
@@ -4,12 +4,11 @@ import { getClasses } from '../apiClient'
 
 const Calendar = () => {
   const classes = getClasses()
-
-  console.log(getClasses())
+console.log(classes)
   return (
     <div>
       <table id='calendar'>
-        <tr>{days.map((day, i) => <th key={i}>{day[0]}</th>)}</tr>
+        <tr>{classes.map((day, i) => <th key={i}>{day[0]}</th>)}</tr>
         <tr>{days.map((Class, j) => <td><h5><ClassDetails classDetails={Class[1]} /></h5></td>)}</tr>
       </table>
     </div>
