fffaaedd0cd9656ea8d4b48a330cb9b89f4c81af
nicole-ilustre <nicoleilustre2292@gmail.com>
2021-04-16 15:40:49 +1200

    fixed getClasses function

--- client/components/Calendar.jsx
+++ client/components/Calendar.jsx
@@ -1,15 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ClassDetails from './ClassDetails'
 import { getClasses } from '../apiClient'
 
 const Calendar = () => {
-  const classes = getClasses()
-console.log(classes)
+  const [classes, setClasses] = useState([])
+  useEffect(() => {
+    getClasses()
+      .then(classes => setClasses(classes))
+  }, [])
+
   return (
     <div>
       <table id='calendar'>
         <tr>{classes.map((day, i) => <th key={i}>{day[0]}</th>)}</tr>
-        <tr>{days.map((Class, j) => <td><h5><ClassDetails classDetails={Class[1]} /></h5></td>)}</tr>
+        {/* <tr>{days.map((Class, j) => <td><h5><ClassDetails classDetails={Class[1]} /></h5></td>)}</tr> */}
       </table>
     </div>
   )
