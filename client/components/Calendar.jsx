c83c9649fda72d6a37126992382f195442c9abb6
nicole-ilustre <nicoleilustre2292@gmail.com>
2021-04-19 17:36:39 +1200

    .map function

--- client/components/Calendar.jsx
+++ client/components/Calendar.jsx
@@ -9,19 +9,36 @@ const Calendar = () => {
       .then(classes => setClasses(classes))
       .catch(e => console.error(e.message))
   }, [])
+  const days = {
+    'Monday': [],
+    'Tuesday': [],
+    'Wednesday': [],
+    'Thursday': [],
+    'Friday': [],
+    'Saturday': [],
+    'Sunday': []
+  }
+
+  Object.keys(days).map(day => {
+    return classes.map(classes => {
+      if (classes.day == day) {
+        days[day].push(classes)
+      }})})
+
+console.log(days)
   return (
     <div>
       <Table inverted>
         <Table.Header>
           <Table.Row>
-            {classes.map((Class, i) => <Table.HeaderCell key={i}>{Class.day}</Table.HeaderCell>)}
+            {Object.keys(days).map((day, i) => <Table.HeaderCell key={i}>{day}</Table.HeaderCell>)}
           </Table.Row>
         </Table.Header>
-        <Table.Body>
+        {/* <Table.Body>
           <Table.Row>
-            {classes.map((Class) => <Table.Cell><h5>{Class.title}</h5><span>{Class.description}</span></Table.Cell>)}<Table.Cell>John</Table.Cell>
+            {classes.map((Class) => <Table.Cell><h5>{Class.title}</h5><span>{Class.description}</span></Table.Cell>)}
           </Table.Row>
-        </Table.Body>
+        </Table.Body> */}
       </Table>
     </div>
   )
