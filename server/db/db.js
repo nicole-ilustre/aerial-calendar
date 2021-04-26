348de6be65a1c5c0bdc92b566d0901482886b7fb
nicole-ilustre <nicoleilustre2292@gmail.com>
2021-04-27 11:13:24 +1200

    add userExist and createUser function

--- server/db/db.js
+++ server/db/db.js
@@ -1,11 +1,22 @@
 const config = require('./knexfile').development
 const database = require('knex')(config)
+const { generateHash } = require('authenticare/server')
 
 module.exports = {
   getClasses,
-  getDays
+  getDays,
+  getUser,
+  userExists,
+  createUser
 }
-
+function userExists (name, db = database) {
+  return db('users')
+    .count('id as n')
+    .where('name', name)
+    .then(count => {
+      return count[0].n > 0
+    })
+}
 function getClasses (db = database) {
   return db('classes').select('title', 'day', 'teacher', 'description', 'day')
 }
@@ -13,4 +24,26 @@ function getClasses (db = database) {
 function getDays (day, db = database) {
   return db('classes').select('title')
     .where('day', day)
+}
+
+function getUser (name, db = database) {
+  return db('users').select()
+    .where('name', name)
+    .first()
+}
+
+function createUser (user, db = database) {
+  const { name, password } = user
+  return userExists(name, db)
+    .then(exists => {
+      if (exists) {
+        return Promise.reject(new Error('Username already taken'))
+      }
+      return null
+    })
+    .then(() => generateHash(password))
+    .then(passwordHash => {
+      return db('users')
+        .insert({ name: name, hash: passwordHash })
+    })
 }
